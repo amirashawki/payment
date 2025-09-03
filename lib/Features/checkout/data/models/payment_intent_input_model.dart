02f0e0564955bb7c7dd2130dc8169417f799b632
Amira Shawki <amirashawki7@gmail.com>
2025-09-03 18:31:28 +0000

    add Payment Intent Input Model

---
+++ lib/Features/checkout/data/models/payment_intent_input_model.dart
@@ -0,0 +1,18 @@
+class PaymentIntentInputModel {
+  final String amount;
+  final String currency;
+ 
+
+  PaymentIntentInputModel({
+    required this.amount,
+    required this.currency,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'amount': amount,
+      'currency': currency,
+    
+    };
+  }
+}
